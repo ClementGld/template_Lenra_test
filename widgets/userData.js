9a3cd6623da602ca47798b804e6d1f8bfbad5ca8
ClementGld <clement.guillaud@lenra.io>
2022-05-24 16:43:58 +0200

    commit

--- widgets/userData.js
+++ widgets/userData.js
@@ -1,10 +1,10 @@
 'use strict'
 
-module.exports = (data, props) => {
+module.exports = (data, props, context) => {
     var userData = data[0]
     return {
         type: "text",
-        value: `Email : ${userData._user.email}`
+        value: JSON.stringify(context)
     }
 }
 
