1ff3f830313702ec7abf03d5a6cde7cc09602ebe
Clément Guillaud <91874264+ClementGld@users.noreply.github.com>
2022-06-14 16:23:06 +0200

    Update userData.js

--- widgets/userData.js
+++ widgets/userData.js
@@ -2,9 +2,6 @@
 
 module.exports = (data, props, context) => {
     var userData = JSON.stringify(context)
-    if (userData == null || userData == undefined) {
-        userData = "No user data"
-    }
     return {
         type: "text",
         value: userData
